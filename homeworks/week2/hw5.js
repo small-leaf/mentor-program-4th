6ce3fc22bad285e02a581c76e21ab873404a8a7d
small_leaf <64968174+small-leaf@users.noreply.github.com>
2020-06-16 21:06:57 +0800

    Update hw5.js

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,22 @@
-function join(arr, concatStr) {
-  
+function join(arr, str){
+    var newarr = [arr[0]];
+    for(var i = 1; i < arr.length;i++){
+        newarr += str + arr[i]
+    }
+    return newarr
 }
+console.log(join([1, 2, 3], ''))
+console.log(join(["a", "b", "c"], "!"))
+console.log(join(["a", 1, "b", 2, "c", 3], ','))
 
-function repeat(str, times) {
-  
+
+
+function repeat(str, num){
+    var newstr = '';
+    for(var i = 0; i < num; i++){
+        newstr += str
+    }
+    return newstr
 }
-
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(repeat('a', 5))
+console.log(repeat('yoyo', 2))
